21ca2b1e3e7f26d74bc57b1375e130f7c0a31b9f
Kai4Sut <kd4accounts@hotmail.com>
2024-12-01 09:16:04 -0500

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by user260677 on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
